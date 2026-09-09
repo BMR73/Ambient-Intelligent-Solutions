7bde9620614dbd6d89523fe02d80f08163d52fda
BMR73 <brianmriggle@hotmail.com>
2026-09-09 14:04:56 -0700

    Refactor getLatestOrder to read from file system

--- api/getLatestOrder/index.js
+++ api/getLatestOrder/index.js
@@ -1,8 +1,21 @@
 module.exports = async function (context, req) {
-    const order = require('../order.json');
+    const fs = require('fs');
+    const path = require('path');
+
+    const filePath = path.join(__dirname, '../order.json');
+
+    if (!fs.existsSync(filePath)) {
+        context.res = {
+            status: 200,
+            body: {}
+        };
+        return;
+    }
+
+    const allOrders = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
     context.res = {
         status: 200,
-        headers: { "Content-Type": "application/json" },
-        body: order
+        body: allOrders
     };
 };
