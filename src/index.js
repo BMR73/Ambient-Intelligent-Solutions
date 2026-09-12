456970cca29c32c6eb05a20d1a7384514f4e8040
BMR73 <brianmriggle@hotmail.com>
2026-09-12 12:53:22 -0700

    Enable HTTP streaming and register functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 const { app } = require("@azure/functions");
 
+// Enable streaming (required for ElevenLabs)
 app.setup({
   enableHttpStream: true
 });
+
+// Register functions
+require("./functions/getLatestOrder");
+require("./functions/orderUpdate");
+
+module.exports = app;
