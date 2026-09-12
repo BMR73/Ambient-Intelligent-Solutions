e8a7196a6ca7dcc20b118955181918a0ff10ef60
BMR73 <brianmriggle@hotmail.com>
2026-09-12 15:49:54 -0700

    Simplify app setup for ElevenLabs streaming
    Condense app setup configuration into a single line.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 const { app } = require("@azure/functions");
 
-// Enable streaming (required for ElevenLabs)
-app.setup({
-  enableHttpStream: true
-});
+app.setup({ enableHttpStream: true });
 
-// Register functions
 require("./functions/getLatestOrder");
 require("./functions/orderUpdate");
-
-module.exports = app;
